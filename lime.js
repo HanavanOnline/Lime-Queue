4a9c7fa7f7ae5d5beedb6d498523dbe77ba0e242
Tyler Hanavan <TylerHanavan@gmail.com>
2016-11-30 23:59:22 -0500

    Version increment for bug fix

--- lime.js
+++ lime.js
@@ -1,5 +1,5 @@
 function Lime(interval = 50) {
-  this.version = "v1.1.1";
+  this.version = "v1.1.2";
   this.routes = [];
   this.timer = new LimeTimer(this).init(interval);
   this.doRequest = function(key, data = "", options = {}) {
